e53666cc05c5ecb753d1483c75b623a6165d0633
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-08-27 23:24:36 -0300

    feat: add basic game mechanics

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import Game from './components/Game';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister();
