426671ff47d9b93e1a368f2ad0231a03fae9ecfe
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-09-21 16:02:34 -0300

    feat: adjust main App file to multi screen

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
-import React, { useState } from 'react'
-import ContactInput from './components/ContactInput'
-import ContactList from './components/ContactList'
-import {
-  StyleSheet,
-  TextInput,
-  Button,
-  Text,
-  FlatList,
-  View
-} from 'react-native'
+
+import React from 'react';
+import ContactsNavigation from './navigation/ContactsNavigation';
 
 export default function App() {
-  const [contactKey, setContactKey] = useState(8)
-  const [contacts, setContacts] = useState([])
-  const addContact = contact => {
-    setContactKey(contactKey + 2)
-    setContacts(contacts => [
-      ...contacts,
-      { key: contactKey.toString(), ...contact }
-    ])
-  }
-
-  const removeContact = key => {
-    setContacts(contacts.filter(el =>
-      el.key !== key))
-  }
-
   return (
-    <View style={ styles.container }>
-      <ContactInput onAddContact={ addContact }/>
-      <ContactList
-        contacts={ contacts }
-        onDeleteContact={ removeContact }
-      />
-    </View>
+    <ContactsNavigation />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 120
-  }
-})
