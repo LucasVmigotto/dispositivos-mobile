0290868f79d63874c10816dcdad1763d6fa28c3a
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-09-25 15:14:24 -0300

    feat: configure main component
    Add state store provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
 import React from 'react';
 import PlacesNavigation from './navigation/PlacesNavigation';
+import placesReducer from './store/placesReducer';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+  places: placesReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk))
 
 export default function App() {
   return (
-    <PlacesNavigation />
+    <Provider store={ store }>
+      <PlacesNavigation />
+    </Provider>
   )
 }
