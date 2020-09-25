c3dddbe9cfbe5ee4a4ea2518f7589ee437feec3b
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-09-25 16:09:11 -0300

    fix: adjust ContactList component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,29 +2,19 @@ import React from 'react'
 import Contact from './Contact'
 import { FlatList, StyleSheet } from 'react-native'
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = ({ contacts }) => {
   return (
     <FlatList
-      style={ styles.itemList }
       data={ contacts }
+      keyExtractor={ contact => contact.id }
       renderItem={
-        contact => (
-          <Contact
-            contact={ contact }
-            onSetContact={ onDeleteContact }
-          />
-        )
+        contact =>
+          <Contact contact={ { ...contact.item } } />
       }
     />
   )
 }
 
-const styles = StyleSheet.create({
-  itemList: {
-    width: '90%',
-    marginTop: 10,
-    alignSelf: 'center'
-  }
-})
+const styles = StyleSheet.create({})
 
 export default ContactList
