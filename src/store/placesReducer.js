4917d0136f4c0c4fbaf9ec45c64fd90131628e1d
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-10-02 20:42:01 -0300

    feat: add list places

--- src/store/placesReducer.js
+++ src/store/placesReducer.js
@@ -1,4 +1,5 @@
 import Place from '../models/Place'
+import { ADD_PLACE, LIST_PLACES } from './placesActions'
 
 const initialState = {
   places: []
@@ -6,16 +7,21 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch(action.type) {
-    case 'ADD_PLACE':
+    case ADD_PLACE:
       return ({
         places: state.places.concat(
           new Place(
-            new Date().toString(),
+            action.placeData.id.toString(),
             action.placeData.name,
             action.placeData.image
           )
         )
       })
+    case LIST_PLACES:
+      return {
+        places: action.places
+          .map(el => new Place(el.id.toString(), el.name, el.image))
+      }
     default:
       return state
   }
