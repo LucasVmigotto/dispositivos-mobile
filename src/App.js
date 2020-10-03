54f85aa77a8a0715d8af1efa685194b48a310c11
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-10-02 22:00:04 -0300

    feat: add database initialize

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import reduxThunk from 'redux-thunk'
 import contactsReducer from './store/contactsReducer';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import { init } from './helpers/db'
+
+init()
+  .then(() => {
+    console.log('Database successully created')
+  })
+  .catch(() => {
+    console.log('An error occurred while creating database')
+  })
 
 const rootReducer = combineReducers({
   contacts: contactsReducer
