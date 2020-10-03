b5a905d32ede539f03b893b71df7a96d6f245f55
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-10-02 22:00:42 -0300

    feat: adjust contacts reducer

--- src/store/contactsReducer.js
+++ src/store/contactsReducer.js
@@ -1,4 +1,5 @@
 import Contact from '../models/Contact'
+import { ADD_CONTACT, LIST_CONTACTS } from './contactsActions'
 
 const initialState = {
   contacts: []
@@ -6,12 +7,27 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch(action.type) {
-    case 'ADD_CONTACT':
+    case ADD_CONTACT:
       return ({
         contacts: state.contacts.concat(
-          new Contact({ ...action.data })
+          new Contact({
+            id: action.data.id.toString(),
+            name: action.data.name,
+            phone: action.data.phone,
+            image: action.data.image
+          })
         )
       })
+    case LIST_CONTACTS:
+      return {
+        contacts: action.contacts
+          .map(el => new Contact({
+            id: el.id.toString(),
+            name: el.name,
+            phone: el.phone,
+            image: el.image
+          }))
+      }
     default:
       return state
   }
