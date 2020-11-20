a93f02e3ecaa5d90685272021dd0fbf2b210d723
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2020-11-20 15:26:52 -0300

    feat: add detail restaurant to navigation

--- Navigation.js
+++ Navigation.js
@@ -2,11 +2,13 @@ import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import AddRestaurant from './views/AddRestaurant'
 import RestaurantView from './views/RestaurantView'
+import DetailRestaurantView from './views/DetailRestaurantView'
 
 const Navigator = createStackNavigator(
   {
     RestaurantView: RestaurantView,
-    AddRestaurant: AddRestaurant
+    AddRestaurant: AddRestaurant,
+    DetailRestaurantView
   }
 )
 
